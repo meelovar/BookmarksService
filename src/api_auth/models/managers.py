5a2772e65f11b872cb697d0558e3c39566ba9cff
meelovar <104781245+meelovar@users.noreply.github.com>
2023-09-01 11:25:11 +0300

    Fix setting is_staff and is_superuser flags

--- src/api_auth/models/managers.py
+++ src/api_auth/models/managers.py
@@ -26,4 +26,4 @@ class UserManager(auth_models.BaseUserManager):
         if extra_fields.get("is_superuser") is not True:
             raise ValueError("Superuser must have is_superuser=True.")
 
-        return self.create_user(email, password)
+        return self.create_user(email, password, **extra_fields)
