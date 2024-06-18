6505f905e9f7198fb0e774645d0c1abfa2faacdb
meelovar <104781245+meelovar@users.noreply.github.com>
2024-06-18 19:38:07 +0300

    Extracted user model relationship to separate variable

--- src/bookmarks/models.py
+++ src/bookmarks/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib import auth
 
+UserModel = auth.get_user_model()
+
 
 class Bookmark(models.Model):
     class LinkType(models.TextChoices):
@@ -15,18 +17,19 @@ class Bookmark(models.Model):
     link = models.URLField(max_length=512)
     link_type = models.CharField(max_length=32, choices=LinkType.choices, default=LinkType.Website)
     preview_image = models.URLField(default="")
-    created_by = models.ForeignKey(auth.get_user_model(), models.CASCADE)
+    created_by = models.ForeignKey(UserModel, models.CASCADE)
     saved_at = models.DateTimeField(auto_now_add=True)
     edited_at = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
         return f"{super().__str__()}: {self.link}"
 
+
 class Collection(models.Model):
     title = models.CharField(max_length=64)
     description = models.CharField(max_length=64)
     bookmarks = models.ManyToManyField(Bookmark, "collections")
-    created_by = models.ForeignKey(auth.get_user_model(), models.CASCADE)
+    created_by = models.ForeignKey(UserModel, models.CASCADE)
     saved_at = models.DateTimeField(auto_now_add=True)
     edited_at = models.DateTimeField(auto_now=True)
 
